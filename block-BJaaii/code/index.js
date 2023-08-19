d79cc592742fb1ef785ff1d31fa418a5e66304a5
KASHISH <kashu80525@gmail.com>
2023-08-19 18:27:24 +0530

    adding codes

---
+++ block-BJaaii/code/index.js
@@ -0,0 +1,27 @@
+
+
+class Stack{
+    constructor(stack){
+        this.stack = stack;
+    }push(){
+
+    }
+    pop(){
+
+    }
+    peek(){
+
+    }
+    reverse(){
+
+    }
+    isEmpty(){
+
+    }
+    displayStack(){
+
+    }
+    get length(){
+
+    }
+}
