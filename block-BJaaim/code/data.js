5e8f8a1ca1b90edc7c45f3043ef66501661d5099
KASHISH <kashu80525@gmail.com>
2023-08-22 16:16:04 +0530

    adding quiz app

---
+++ block-BJaaim/code/data.js
@@ -0,0 +1,38 @@
+const quizCollection = [
+    {
+        title: " Who is the father of Computers?",
+        options: [
+            "James Gosling" ,
+             "Charles Babbage" ,
+              "Dennis Ritchie" , 
+              "Bjarne Stroustrup",
+        ] ,
+        correctAnswer: 1,
+    },
+    {
+        title: "Which of the following is the correct abbreviation of COMPUTER?",
+        options: ["Commonly Occupied Machines Used in Technical and Educational Research" ,
+         " Commonly Operated Machines Used in Technical and Environmental Research " , 
+         "Commonly Oriented Machines Used in Technical and Educational Research ",
+          "Commonly Operated Machines Used in Technical and Educational Research "]  , 
+        correctAnswer: 3,
+    },
+    {
+        title: " What is the full form of CPU?",
+        options: ["Computer Processing Unit" ,
+         " Computer Principle Unit" , 
+         " Central Processing Unit ", 
+         " Control Processing Unit "] , 
+        correctAnswer: 2,
+    },
+    {
+        title: "Which of the following computer language is written in binary codes only?",
+        options: ["pascal" , "machine language " , " C", "C# "] , 
+        correctAnswer: 1,
+    },
+    {
+        title: "Which of the following is the brain of the computer?",
+        options: ["Central Processing Unit" , " Memory" , " Arithmetic and Logic unit ", " Control unit"] , 
+        correctAnswer: 0
+    },
+]
